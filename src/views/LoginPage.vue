323728d9af3d7ec46e83046d740e98c2ebfc6738
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-10-06 18:50:54 -0500

    Styled login page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,15 +1,15 @@
 <template>
-    <form class="" @submit.prevent>
+    <form class="m-12 " @submit.prevent>
      <div>
-        <label for="username" class="">Username</label>
+        <label for="username" class="block mb-4 font-extrabold">Username</label>
         <input id="username" v-model="username" type="text" placeholder="Username">
      </div>  
       <div>
-        <label for="password" class="">Password</label>
+        <label for="password" class="block mb-4 font-extrabold">Password</label>
         <input id="pasword" v-model="password" type="password" placeholder="Password">
      </div>
      
-     <buton @click="logUserIn" class="">LOGIN</buton>
+     <buton @click="logUserIn" class="bg-green-500 mt-6 px-2 hover:bg-blue-800 hover:text-white">LOGIN</buton>
     </form>
 </template>
 
