693a2360c42f391ea35f7ee1bd564d2367295a64
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-10-06 17:21:34 -0500

    CardDetails Done

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { faker } from '@faker-js/faker'
-const fullname = faker.person.fullName()
+
 const firstname = faker.person.firstName()
 const lastname = faker.person.lastName()
+const fullname = faker.person.fullName({firstName: firstname, lastName:lastname})
 const job_title = faker.person.jobTitle()
 const bio = faker.lorem.lines(2)
 const username = faker.internet.userName({firstName: firstname, lastName:lastname}).toLowerCase()
@@ -10,13 +11,15 @@ const username = faker.internet.userName({firstName: firstname, lastName:lastnam
 </script>
 
 <template>
-<!-- {{ fullname }} <br /> -->
-<div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
-    <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'beaches'})" class="rounded-t-xl" />
-    <div class="p-4">
-        <p class="mb-2 text-3xl font-bold">{{ fullname }}</p>
-        <p class="mb-4 font-semibold text-gray-700">{{ job_title }}</p>
-        <p class="mb-4 font-medium text-gray-700">{{ bio }}</p>
-    </div>
-</div>
+    <!-- {{ fullname }} <br /> -->
+    <RouterLink :to="{name: 'CardDetails', params: {id:username}}">
+        <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
+             <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'beaches'})" class="rounded-t-xl" />
+             <div class="p-4">
+                 <p class="mb-2 text-3xl font-bold">{{ fullname }}</p>
+                 <p class="mb-4 font-semibold text-gray-700">{{ job_title }}</p>
+                <p class="mb-4 font-medium text-gray-700">{{ bio }}</p>
+             </div>
+        </div>
+    </RouterLink>
 </template>
