3e61027e49ee0aabf8676257ddf4495d354d811c
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-10-06 18:43:26 -0500

    login successful

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,3 +1,43 @@
 <template>
-    Login
+    <form class="" @submit.prevent>
+     <div>
+        <label for="username" class="">Username</label>
+        <input id="username" v-model="username" type="text" placeholder="Username">
+     </div>  
+      <div>
+        <label for="password" class="">Password</label>
+        <input id="pasword" v-model="password" type="password" placeholder="Password">
+     </div>
+     
+     <buton @click="logUserIn" class="">LOGIN</buton>
+    </form>
 </template>
+
+
+<script setup>
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+import { useAuth } from '../composables/useAuth'
+
+const { login, logout } = useAuth()
+
+const router = useRouter()
+const route = useRoute()
+
+const username = ref('')
+const password = ref('')
+
+const logUserIn = async () => {
+    if (await login(username.value, password.value)) {
+        if (route.query.redirect) {
+            router.push(route.query.redirect)
+        } else {
+            router.push({name: 'SettingsPage'})
+        }
+    } else {
+        logout()
+    }
+}
+
+</script>
