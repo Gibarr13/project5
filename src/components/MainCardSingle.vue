d79a67a18c6c3d7b7d07ea36718c85fa9a45e8ed
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-10-06 17:56:32 -0500

    Installed firebase

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -14,7 +14,7 @@ const username = faker.internet.userName({firstName: firstname, lastName:lastnam
     <!-- {{ fullname }} <br /> -->
     <RouterLink :to="{name: 'CardDetails', params: {id:username}}">
         <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
-             <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'beaches'})" class="rounded-t-xl" />
+             <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'mountains'})" class="rounded-t-xl" />
              <div class="p-4">
                  <p class="mb-2 text-3xl font-bold">{{ fullname }}</p>
                  <p class="mb-4 font-semibold text-gray-700">{{ job_title }}</p>
---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA29wyEO86mtlWOcURl7IDJgZWqu1EI80E",
+  authDomain: "project5-d57c0.firebaseapp.com",
+  projectId: "project5-d57c0",
+  storageBucket: "project5-d57c0.appspot.com",
+  messagingSenderId: "49870074158",
+  appId: "1:49870074158:web:2727263f09ec84f9adcabf"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
